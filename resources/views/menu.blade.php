e371e331a611fef13a82044d4c38b285941e5290
Marco Boom <marco@quest-media.nl>
2017-01-26 15:19:17 +0100

    Dynamic menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,46 +1,26 @@
 <ul class="nav navbar-nav">
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-			Collection <span class="caret"></span>
-		</a>
 
-		<ul class="dropdown-menu" role="menu">
-			<li>
-				<a href="">Objects</a>
-				<a href="">Artists</a>
-				<a href="">Selections</a>
-				<a href="">Stories</a>
-				<a href="">Research</a>
+	@foreach (config('admin.menu') as $main)
+
+		@if (!empty($main['children']))
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+					{{ $main['name'] }} <span class="caret"></span>
+				</a>
+
+				<ul class="dropdown-menu" role="menu">
+					@foreach ($main['children'] as $sub)
+						<li>
+							<a href="{{ $sub['url'] }}">{{ $sub['name'] }}</a>
+						</li>
+					@endforeach
+				</ul>
 			</li>
-		</ul>
-	</li>
 
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-			Content <span class="caret"></span>
-		</a>
-
-		<ul class="dropdown-menu" role="menu">
+		@else
 			<li>
-				<a href="">Pages</a>
-				<a href="">Blogs</a>
-				<a href="">News</a>
-				<a href="">Expositions</a>
+				<a href="{{ $main['url'] }}">{{ $main['name'] }}</a>
 			</li>
-		</ul>
-	</li>
-
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-			System <span class="caret"></span>
-		</a>
-
-		<ul class="dropdown-menu" role="menu">
-			<li>
-				<a href="">Release cache</a>
-				<a href="">Media</a>
-				<a href="">Users</a>
-			</li>
-		</ul>
-	</li>
+		@endif
+	@endforeach
 </ul>
