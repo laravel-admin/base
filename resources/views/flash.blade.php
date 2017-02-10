bd64a0b1c2ae5cd6afbd5f401ed527593b6df152
Rowdy Klijnsmit <rowdy.klijnsmit@in10.nl>
2017-02-10 12:15:27 +0100

    Align alert inside the container

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -3,18 +3,23 @@
         @include('flash::modal', ['modalClass' => 'flash-modal', 'title' => Session::get('flash_notification.title'), 'body' => Session::get('flash_notification.message')])
     @else
         <div class="alert alert-{{ Session::get('flash_notification.level') }} alert--active disapear">
-            {{ Session::get('flash_notification.message') }}
+            <div class="container">
+                {{ Session::get('flash_notification.message') }}
             </div>
+        </div>
     @endif
 
 @elseif (isset($errors) && $errors->first())
 	<div class="alert alert-danger alert--active disapear">
-        {{ $errors->first() }}
+        <div class="container">
+            {{ $errors->first() }}
+        </div>
     </div>
 
 @elseif (session('status'))
 	<div class="alert alert-success alert--active disapear">
-        {{ session('status') }}
+        <div class="container">
+            {{ session('status') }}
         </div>
-
+    </div>
 @endif
