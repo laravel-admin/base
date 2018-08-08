96e86363d186034d8d45c63b66fb03100dd02ec7
Jakob Buis <jakob.buis@in10.nl>
2018-08-08 11:37:30 +0200

    fix logic that placed submenu indicators on level 2 items that did not have submenu's configured

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -15,18 +15,24 @@
             @if ($item['children']->count() > 0)
                 <ul class="dropdown-menu" role="multi-level">
                     @foreach ($item['children'] as $child)
-                        <li class="dropdown-submenu">
-                            <a href="{{ $child['url'] }}">{{ $child['name'] }}</a>
-                            @if ($child['children']->count() > 0)
-                                <ul class="dropdown-menu" role="multi-level">
-                                    @foreach ($child['children'] as $grandchild)
-                                        <li>
-                                            <a href="{{ $grandchild['url'] }}">{{ $grandchild['name'] }}</a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            @endif
-                        </li>
+                        @if ($child['children']->count() > 0)
+                            <li class="dropdown-submenu">
+                                <a href="{{ $child['url'] }}">{{ $child['name'] }}</a>
+                                @if ($child['children']->count() > 0)
+                                    <ul class="dropdown-menu" role="multi-level">
+                                        @foreach ($child['children'] as $grandchild)
+                                            <li>
+                                                <a href="{{ $grandchild['url'] }}">{{ $grandchild['name'] }}</a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
+                            </li>
+                        @else
+                            <li>
+                                <a href="{{ $child['url'] }}">{{ $child['name'] }}</a>
+                            </li>
+                        @endif
                     @endforeach
                 </ul>
             @endif
